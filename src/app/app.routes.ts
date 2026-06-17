4b9f61d48ce7bdb9b4cc80c7e6e9774bcd2c1965
Hossein heydarpour <hosseinheydarpour1@gmail.com>
2026-06-17 10:47:52 +0330

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UserCardComponent } from './features/user-card/user-card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		component: UserCardComponent,
+	},
+];
