a055992b7b13d25b05c1001108284bcc6656dec6
Hossein heydarpour <hosseinheydarpour1@gmail.com>
2026-06-17 09:35:17 +0330

    feat: add environments for the app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
+import { APP_CONFIG } from './core/config/app-config.token';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+	providers: [
+		provideZoneChangeDetection({ eventCoalescing: true }),
+		provideRouter(routes),
+		{ provide: APP_CONFIG, useValue: environment },
+	],
 };
