2c25602e0c0882e2f959b543833722960d80ee1a
Hossein heydarpour <hosseinheydarpour1@gmail.com>
2026-06-17 09:27:17 +0330

    feat: add assets for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{html,js}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				customGreen: 'hsl(75, 94%, 57%)',
+				customWhite: 'hsl(0, 0%, 100%)',
+				grey: {
+					700: 'hsl(0, 0%, 20%)',
+					800: 'hsl(0, 0%, 12%)',
+					900: 'hsl(0, 0%, 8%)',
+				},
+			},
+			fontFamily: {
+				sans: ['Inter', 'sans-serif'], // This makes Inter your default sans font
+				inter: ['Inter', 'sans-serif'], // Optional: lets you use utility class 'font-inter'
+			},
+		},
 	},
 	plugins: [],
 };
